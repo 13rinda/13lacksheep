efa6baf36c5378146fd0d8081b4b8b9a4f81067f
13rinda <brindarenekah@gmail.com>
2022-11-22 22:58:05 -0500

    site key added to index page

--- includes/scripts.php
+++ includes/scripts.php
@@ -21,7 +21,7 @@
         function onClick(e) {
           e.preventDefault();
           grecaptcha.ready(function() {
-            grecaptcha.execute('6LfciCUjAAAAAOgp6mqTQQLC5zuuVujIp6351gYH', {action: 'submit'}).then(function(token) {
+            grecaptcha.execute('6LeKrikjAAAAAMueTepO9B659RVPiES_uJ9Ja3CE', {action: 'submit'}).then(function(token) {
                 // Add your logic to submit to your backend server here.
             });
           });
